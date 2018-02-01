c9d39fd3fcb3a3c7f2af0333bb970acf0e4e1e90
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2018-02-01 15:43:13 +0000

    Add modal UI delius code

--- src/containers/app.js
+++ src/containers/app.js
@@ -2,23 +2,58 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux' ;
 import { bindActionCreators } from 'redux';
 
-//import { Score } from 'meld-client/src/containers/score';
-import { fetchGraph } from 'meld-client/src/actions/index';
-
-const MEIManifestation = "meldterm:MEIManifestation";
+import Score from 'meld-client/src/containers/score';
+import Modal from 'meld-client/src/containers/modalUI';
+import { fetchGraph } from '../../../meld-client/src/actions/index';
+import { setMode, clearConstituents, elementClicked } from '../../../meld-client/src/actions/modalUI';
+import { modes } from '../../config/deliusModes';
 
 class App extends Component { 
 	constructor(props) {
 		super(props);
-		this.state = { currentMotif: this.props.motif || false };
+		this.state = { 
+			currentMotif: this.props.motif || false,
+			modes: modes
+		 };
 	}
 	componentDidMount() { 
 		if(this.props.graphUri) { 
 			const graphUri = this.props.graphUri;
 			this.props.fetchGraph(graphUri);
 		}
+		console.log("I've found these notes: ",document.querySelectorAll('.note'));
 	}
+	componentWillReceiveProps(nextProps) { 
+		// this is where we do app-specific logic for the modal UI
+		if (this.props.modalUI.mode == "baseMode" && nextProps.modalUI.constituents.has("dynamics")) {
+			// user has selected dynamics - clear selections, and switch modes
+			this.props.clearConstituents();
+			this.props.setMode("dynamicsMode");
+		}
+	}
+
 	render() { 
-		return (<div> Loading...  </div>);
+		return (
+			<div> 
+					<link rel="stylesheet" href="style/modalUI.css" type="text/css" />
+					<Modal modes={this.state.modes} orientation="wide"/> 
+					<Score uri="http://meld.linkedmusic.org/mei/Late_Swallows-dolet-musescore-II.mei" 
+						onClick={(e) =>  this.handleScoreClick(e) } ref="score" />
+			</div>
+		)
+	}
+	
+	handleScoreClick(e) { 
+		console.log("score clicked: ", e);
 	}
 }
+
+function mapStateToProps({ modalUI }) {
+	return { modalUI }
+}
+
+function mapDispatchToProps(dispatch) { 
+	return bindActionCreators({ setMode, clearConstituents }, dispatch);
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
