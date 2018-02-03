faf9b2ace81e6fe5e560772ff63d73523b2ba3c2
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2018-02-03 00:30:12 +0000

    Track how many modalUI.elements (notes) are selected; when > 2, remove the oldest from selection; highlight the currently selected notes

--- src/actions/deliusActions.js
+++ src/actions/deliusActions.js
@@ -1,17 +1,17 @@
 import ReactDOM from 'react-dom';
 
-export function decorateNotes(component) {  
+export function attachClickHandlerToNotes(scoreComponent) {  
 	// horrible hack -- 
 	// we have to dive into the browser DOM to get at the score SVG.
-	// When componetDidMount is called, all the react components have
+	// When componetDidMount is called, all the react scoreComponents have
 	// rendered (i.e. we have the <Score>-div's)...
-	// ...but the non-react components aren't done yet.
+	// ...but the non-react scoreComponents aren't done yet.
 	// Since React doesn't know about our poor notes directly,
 	// we have to wait a second for them to appear in the browser DOM.
-	const element = ReactDOM.findDOMNode(component);
+	const scoreElement = ReactDOM.findDOMNode(scoreComponent);
 	return (dispatch) => { 
 		setTimeout(() => {	
-			let notes = element.querySelectorAll(".note");
+			const notes = scoreElement.querySelectorAll(".note");
 			Array.prototype.map.call(notes, function(n) { 
 				n.onclick = function(e) { 
 					dispatch({ 
@@ -20,7 +20,26 @@ export function decorateNotes(component) {
 					})
 			 	}
 			});
-			return { type: "NOTES_DECORATED" }
-		}, 1000)
+			return { type: "CLICK_HANDLERS_ATTACHED" }
+		}, 3000)
 	}
+}
+
+export function decorateNotes(scoreComponent, selectedElements) {
+	// (relating to horrible timeout hack above...)
+	// this one is called from componentDidUpdate 
+	// by this time, the notes have already been rendered
+	// so no need for horrible hack here.
+	const scoreElement = ReactDOM.findDOMNode(scoreComponent);
+	const notes = scoreElement.querySelectorAll(".note");
+	Array.prototype.map.call(notes, function(n) { 
+		// mark any notes that are selected in the modal UI
+		// remove any left-over demarkations of now-deselected notes
+		if(selectedElements.includes(n.getAttribute("id"))) {
+			n.classList.add("active");
+		} else { 
+			n.classList.remove("active");
+		}
+	});
+	return {type: "NOTES_DECORATED"};
 }
--- src/containers/app.js
+++ src/containers/app.js
@@ -6,8 +6,8 @@ import { bindActionCreators } from 'redux';
 import Score from 'meld-client/src/containers/score';
 import Modal from 'meld-client/src/containers/modalUI';
 import { fetchGraph } from '../../../meld-client/src/actions/index';
-import { setMode, clearConstituents, elementClicked } from '../../../meld-client/src/actions/modalUI';
-import { decorateNotes } from '../actions/deliusActions';
+import { setMode, clearConstituents, clearElements, popElements } from '../../../meld-client/src/actions/modalUI';
+import { attachClickHandlerToNotes, decorateNotes } from '../actions/deliusActions';
 import { modes } from '../../config/deliusModes';
 
 class App extends Component { 
@@ -23,25 +23,37 @@ class App extends Component {
 			const graphUri = this.props.graphUri;
 			this.props.fetchGraph(graphUri);
 		}
-		
-		this.props.decorateNotes(this.scoreElement)
+		this.props.attachClickHandlerToNotes(this.scoreComponent, this.props.modalUI.elements)
 	}
 	componentWillReceiveProps(nextProps) { 
 		// this is where we do app-specific logic for the modal UI
+		// RULES
+		// 1. If more than 2 note elements selected, throw out the oldest element
+		// 		(only makes sense to have up to 2 selected for Delius annotation)
+		if(nextProps.modalUI.elements.length > 2) { 
+			this.props.popElements();
+		}
+		// 2. If dynamics is clicked in baseMode, switch to dynamicsMode
 		if (this.props.modalUI.mode == "baseMode" && nextProps.modalUI.constituents.has("dynamics")) {
 			// user has selected dynamics - clear selections, and switch modes
 			this.props.clearConstituents();
 			this.props.setMode("dynamicsMode");
 		}
+
+	}
+
+	componentDidUpdate(nextProps) { 
+		// update note classes (ensure only selected ones are highlighted)
+		this.props.decorateNotes(this.scoreComponent, this.props.modalUI.elements);
+		console.log("modalUI elements", this.props.modalUI.elements); 
 	}
 
 	render() { 
-		console.log("My props: ", this.props);
 		return (
 			<div> 
 					<link rel="stylesheet" href="style/modalUI.css" type="text/css" />
 					<Modal modes={this.state.modes} orientation="wide"/> 
-					<Score uri="http://meld.linkedmusic.org/mei/Late_Swallows-dolet-musescore-II.mei" ref={(score) => {this.scoreElement = score}} />
+					<Score uri="http://meld.linkedmusic.org/mei/Late_Swallows-dolet-musescore-II.mei" ref={(score) => {this.scoreComponent = score}} />
 			</div>
 		)
 	}
@@ -52,7 +64,14 @@ function mapStateToProps({ modalUI }) {
 }
 
 function mapDispatchToProps(dispatch) { 
-	return bindActionCreators({ setMode, clearConstituents, decorateNotes }, dispatch);
+	return bindActionCreators({ 
+		attachClickHandlerToNotes,
+		clearConstituents, 
+		clearElements,
+		decorateNotes,
+		popElements,
+		setMode 
+	}, dispatch);
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(App);
