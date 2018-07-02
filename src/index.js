a925dfe4da048c7d0dee0bcd373dd9e133354723
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2018-07-02 12:19:29 +0100

    fix reducer import (again)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import thunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise';
 import { Router, Route, browserHistory } from 'react-router'
 
-import reducers from 'meld-clients-core/src/reducers';
+import { reducers } from 'meld-clients-core/src/reducers';
 import App from './containers/app';
 
 const createStoreWithMiddleware = applyMiddleware(thunk, ReduxPromise)(createStore);
