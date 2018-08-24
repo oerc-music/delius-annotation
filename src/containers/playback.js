01d0df87c649d07bc3a8debe32b93f046abb827a
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2018-08-24 11:53:25 +0100

    begin implementing time sensitivity and traversal functionality

--- src/containers/playback.js
+++ src/containers/playback.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import { connect } from 'react-redux' ;
 import { bindActionCreators } from 'redux';
+import MediaPlayer from 'meld-clients-core/src/components/mediaPlayer'
 
 import Score from 'meld-clients-core/src/containers/score';
 import Modal from 'meld-clients-core/src/containers/modalUI';
-import { fetchSessionGraph } from 'meld-clients-core/src/actions/index';
+import { traverse, setTraversalObjectives, checkTraversalObjectives } from 'meld-clients-core/src/actions/index';
 import { setMode, clearConstituents, clearElements, popElements } from 'meld-clients-core/src/actions/modalUI';
 import { attachClickHandlerToNotes, attachClickHandlerToAnnotationGlyphs, decorateNotes, generateCursorBoxes, hideCursorBoxes, showCursorBoxes, unselectCursor } from '../actions/deliusActions';
 import { postAnnotation} from 'meld-clients-core/src/actions/index'
@@ -24,15 +25,33 @@ class Playback extends Component {
 		this.clearCursor= this.clearCursor.bind(this);
 	}
 
+	componentWillMount() { 
+		this.props.setTraversalObjectives([
+			{
+				"http://www.w3.org/ns/oa#annotatedAt": ""
+			}
+
+		]);
+	}
 	componentDidMount() { 
 		console.log("PROPS: ", this.props);
 		if(this.props.route.graphUri) { 
 			const graphUri = this.props.route.graphUri;
 			console.log("GRAPH: ", graphUri);
-			this.props.fetchSessionGraph(graphUri);
+			this.props.traverse(graphUri);
 		}
 		// generate boxes for measures (and adorn with cursor click-handler)
 		this.props.generateCursorBoxes(this.scoreComponent);
+	}
+	
+	componentDidUpdate(prevProps, prevState) {
+		console.log("Did update!", prevProps, this.props);
+		if("graph" in prevProps) { 
+			// check our traversal objectives if the graph has updated
+			if(prevProps.graph.graph.length !== this.props.graph.graph.length) { 
+				this.props.checkTraversalObjectives(this.props.graph.graph, this.props.graph.objectives);
+			}
+		}
 	}
 
 	clearCursor() {
@@ -50,6 +69,7 @@ class Playback extends Component {
 		return (
 			<div> 
 					<link rel="stylesheet" href="style/modalUI.css" type="text/css" />
+					<MediaPlayer uri="/DELIUS_EVENT.mp3" />;
 					<Score uri="/Late Swallows-dolet-musescore-II.mei" options={{pageWidth:19000}} ref={(score) => {this.scoreComponent = score}} />
 			</div>
 		)
@@ -73,7 +93,9 @@ function mapDispatchToProps(dispatch) {
 		popElements,
 		setMode,
 		showCursorBoxes,
-		fetchSessionGraph,
+		traverse,
+		setTraversalObjectives,
+		checkTraversalObjectives,
 		unselectCursor
 	}, dispatch);
 }
